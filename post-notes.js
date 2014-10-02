1aebda0571c98c5db3a0052ae23d9f5cb67ba2df
kbooe <kbooe@iastate.edu>
2014-10-02 00:58:18 -0500

    Got fancy editor to post and pull from firebase
    accomplished by inserting kim's html into Matt's html skeleton. keep
    both editor.js and post-notes.js files.

--- post-notes.js
+++ post-notes.js
@@ -1,6 +1,6 @@
 //variables to grab the thoughts and display them
 	var currentThought = document.getElementById('currentThought'),
-		txtNewThought = document.getElementById('txtNewThought'),
+		txtNewThought = document.getElementById('editor-textarea'),
 		btUpdateNotes = document.getElementById('btUpdateNotes'),
 		
 		//creates a conection to firebase
@@ -27,7 +27,8 @@
 
 		//updates the firebase when button is clicked
 		btUpdateNotes.addEventListener('click', function() {
-			currentThoughtRef.push(txtNewThought.value);
+			$("#editor-textarea").val($("#editor-iFrame").contents().find("body").html());
+            currentThoughtRef.push(txtNewThought.value);
 			currentThoughtRef.set(txtNewThought.value);
 			txtNewThought.value = '';
 		});		
