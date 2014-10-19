b69735377b6d5104322469d9c5eec0c022880588
kbooe <kbooe@iastate.edu>
2014-10-19 15:52:31 -0500

    Pushing the post thought button posts the thought to the user's notes and the class's notes in Firebase.

--- JS/post-notes.js
+++ JS/post-notes.js
@@ -1,28 +1,20 @@
-//variables to grab the thoughts and display them
-  var currentThought = document.getElementById('currentThought'),
-    txtNewThought = document.getElementById('editor-textarea'),
-    btUpdateNotes = document.getElementById('btUpdateNotes'),
+//updates the firebase when button is clicked
+btUpdateNotes.addEventListener('click', function() {
+    // get text from editor box
+    var textToUpload = $("#editor-iFrame").contents().find("body").html();
     
-    //creates a conection to firebase
-    rootRef = new Firebase('https://opennote.firebaseio.com'),
+    var userThoughtRef = rootFBRef.child("users").child(currentUser.userId).child("classes").child(currentClass.userClassId).child("notes").child(currentNote).child("thoughts");
     
-    //sets the current message to the child of the root
-    currentThoughtRef = rootRef.child('currentThought'),
     
-
+    var d = new Date();
+    var thoughtToUpload = {
+        noteHTML: textToUpload,
+        user: currentUser.userId,
+        timeAdded: d.getTime()
+    }
     
-    //used to store the last input text
-    outputString = '';
-
-    //updates the firebase when button is clicked
-    btUpdateNotes.addEventListener('click', function() {
-      $("#editor-textarea").val($("#editor-iFrame").contents().find("body").html());
-            currentThoughtRef.push(txtNewThought.value);
-      currentThoughtRef.set(txtNewThought.value);
-      txtNewThought.value = '';
-    });   
-
-    currentThoughtRef.on('value', function(snapshot) {
-      currentThought.innerText = snapshot.val();
-    });   
+    var classThoughtRef= rootFBRef.child("universities").child(currentUser.university).child("classes").child(currentClass.classId).child("thoughts");
+    
+    classThoughtRef.push(thoughtToUpload);
+});   
 
