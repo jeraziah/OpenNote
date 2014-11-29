87b9c22d1a0f88349712c867928b05031a5882b0
Matt <mszpak@iastate.edu>
2014-11-29 17:28:00 -0600

    just a preview email modal placeholder

--- JS/Contact_modal.js
+++ JS/Contact_modal.js
@@ -2,3 +2,8 @@
 $('#navOptionContact').click(function() {
 	$('#contactModal').modal('show');
 });
+
+$('#contact_Send').click(function() {
+    $('#contactModal').modal('hide');
+    $('#previewContactEmailModal').modal('show');
+});
